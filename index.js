00a50ae791b237173f7ee98290b76719cd22fbe9
RADIOdemon-alt <byzarywsaw1@gmail.com>
2025-10-03 23:55:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import fetch from 'node-fetch';
 import { fileURLToPath } from 'url';
 import path from 'path';
+
 import tools_tr from './routes/tools-tr.js';
 
 const app = express();
@@ -9,31 +11,15 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// لتحويل JSON تلقائي
-app.use(express.json());
 
-// تقديم الملفات الثابتة
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
-// مسارات API
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 app.use('/api/tr', tools_tr);
 
-// صفحة تسجيل الدخول
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// صفحة الهوم
-app.get('/page/home', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'page', 'home', 'index.html'));
-});
-
-// أي مسار تاني يرجع على طول للوجت (login)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// تشغيل السيرفر
 app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
