847e1b090d8c7e941234d62e4a59df4bf4d17718
RADIOdemon-alt <byzarywsaw1@gmail.com>
2025-10-03 16:52:41 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// استيراد الروتات
+import tools_tr from './routes/tools-tr.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// تعريف __dirname في ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// الملفات الثابتة (static)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// الصفحة الرئيسية
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'page', 'login', 'index.html'));
+});
+
+// مسارات API
+app.use('/api/tr', tools_tr);
+
+// تشغيل السيرفر
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
