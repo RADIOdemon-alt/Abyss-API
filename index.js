1b2ae8f37f2e4efd1b056b0bc40a7498c3d879da
RADIOdemon-alt <byzarywsaw1@gmail.com>
2025-10-12 23:17:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import fetch from 'node-fetch';
 import { fileURLToPath } from 'url';
 import path from 'path';
-
+//-------------------------------------------------------
 import tools_tr from './routes/tools-tr.js';
 import pinterest from './routes/download-pinterest.js';
 import tiktok from './routes/download-tiktok.js';
@@ -11,20 +11,22 @@ import image_edit from './routes/image-edit.js';
 import pinvid from './routes/download-pinvid.js';
 import pinvid_dl from './routes/download-pinvid-dl.js';
 import cat_art from './routes/Ai-cat-art.js';
+import search_applemusic from './routes/search-applemusic.js';
+//------------------------------------------------------- 
 const port = process.env.PORT || 3000;
-
+//------------------------------------------------------
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+//------------------------------------------------------
 const app = express();
-
+//------------------------------------------------------
 app.use(express.json());
 app.use(express.static(__dirname));
-
+//------------------------------------------------------
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
-
+//------------------------------------------------------
 app.use('/api/tr', tools_tr);
 app.use('/api/pinterest', pinterest);
 app.use('/api/tiktok', tiktok);
@@ -33,6 +35,8 @@ app.use('/api/nano_banana', image_edit);
 app.use('/api/pin_vid', pinvid);
 app.use('/api/pinvid_dl', pinvid_dl);
 app.use('/api/cat_art', cat_art);
+app.use('/api/search_applemusic', search_applemusic);
+//------------------------------------------------------
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
