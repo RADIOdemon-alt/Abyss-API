223b548f7076a60d7a8672c582653001071e1796
Sayed Shawaza <ssawaza30@gmail.com>
2025-10-21 18:42:32 +0300

    Update By «-  Shawaza  -» :   setupPages.js

---
+++ src/lib/setupPages.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pageDir = (name) => {
+    return path.join(__dirname, '..', 'public', 'html', `${name}.html`)
+};
+
+function setupPages(app, options = {}) {
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+  
+};
+
+export default setupPages;
