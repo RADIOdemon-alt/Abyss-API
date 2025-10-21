b2a7b92410d4020ec5bd2869ff0ce21ea174bff0
Sayed Shawaza <ssawaza30@gmail.com>
2025-10-21 18:36:30 +0300

    Update By «-  Shawaza  -» :   app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+
+import setupApp from './main.js';
+import setupPages from './src/lib/setupPages.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+ setupPages(app);
+await setupApp(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+export default app;
