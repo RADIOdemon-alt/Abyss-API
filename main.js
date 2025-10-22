24641bc1118340f1de0c0efd6c0b016df89a1206
RADIOdemon-alt <byzarywsaw1@gmail.com>
2025-10-22 08:59:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 import express from "express";
 import { routeLoader } from "./src/lib/routesLoader.js";
-import runCodeRoute from "./src/routes/api/tools/run_code.js"; // 👈 استيراد المسار يدوي
-
 async function setupApp(app, options = {}) {
 
   await routeLoader.loadRoutes(app, "api", "/api/v1");
-
-  
-  app.use("/api/v1/tools/run_code", runCodeRoute);
 
 }
 
